185fbce7e9b8b050a25e026925bcb8fbae5faf70
Soubhagya Kumar Nayak <53268143+SoubhagyaKumaNayak@users.noreply.github.com>
2021-10-01 13:06:00 +0530

    Update Sort.java

--- test/src/Sort.java
+++ test/src/Sort.java
@@ -1,41 +1,42 @@
 public class Sort {
-    public static void mergeSort(Comparable[] a) {
-        Comparable[] tmpArray = new Comparable[a.length];
-        mergeSort(a, tmpArray, 0, a.length - 1);
+    public static void mergeSort(int[] a) {
+        int[] tmpA = new int[a.length];
+        mergeSort(a, tmpA, 0, a.length - 1);
     }
 
-    private static void mergeSort(Comparable[] a, Comparable[] tmpArray,
+    private static void mergeSort(int[] a, int[] tmpA,
             int left, int right) {
         if (left < right) {
             int center = (left + right) / 2;
-            mergeSort(a, tmpArray, left, center);
-            mergeSort(a, tmpArray, center + 1, right);
-            merge(a, tmpArray, left, center + 1, right);
+            merge(a, tmpA,left, center + 1, right);
+            mergeSort(a, tmpA, left, center);
+            mergeSort(a, tmpA, center + 1, right);
+            
         }
     }
 
-    private static void merge(Comparable[] a, Comparable[] tmpArray,
+    private static void merge(int[] a, int[] tmpA,
             int leftPos, int rightPos, int rightEnd) {
         int leftEnd = rightPos - 1;
         int tmpPos = leftPos;
-        int numElements = rightEnd - leftPos + 1;
+        int numEle = rightEnd - leftPos + 1;
 
         // Main loop
         while (leftPos <= leftEnd && rightPos <= rightEnd)
             if (a[leftPos].compareTo(a[rightPos]) <= 0)
-                tmpArray[tmpPos++] = a[leftPos++];
+                tmpA[tmpPos++] = a[leftPos++];
             else
-                tmpArray[tmpPos++] = a[rightPos++];
+                tmpA[tmpPos++] = a[rightPos++];
 
         while (leftPos <= leftEnd)
-            tmpArray[tmpPos++] = a[leftPos++];
+            tmpA[tmpPos++] = a[leftPos++];
 
         while (rightPos <= rightEnd)
-            tmpArray[tmpPos++] = a[rightPos++];
+            tmpA[tmpPos++] = a[rightPos++];
 
         // Copy tmpArray back
         for (int i = 0; i < numElements; i++, rightEnd--)
-            a[rightEnd] = tmpArray[rightEnd];
+            a[rightEnd] = tmpA[rightEnd];
     }
 
     public static void main(String[] args) {
